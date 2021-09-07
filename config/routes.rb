12e17cfb378663024d549f876445b13c546ebd1b
Jose <josedparrales@gmail.com>
2021-09-07 11:50:40 -0500

    firsts steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "hola", to: "main#hello"
+  root to: "main#hello"
+  
+  get "saludo", to: "main#hi"
+  post "hi", to: "main#greetings"
 end
