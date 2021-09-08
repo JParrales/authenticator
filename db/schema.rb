34195f77548e116fc96dcd71e0ddb590d2d1fd0e
Jose <josedparrales@gmail.com>
2021-09-08 07:21:52 -0500

    REST

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_213652) do
+ActiveRecord::Schema.define(version: 2021_09_08_120832) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_213652) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
