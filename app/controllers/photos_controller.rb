a58ab478d149a262f092711a450579576dce3187
Jose <josedparrales@gmail.com>
2021-09-10 14:11:21 -0500

    CRUD

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,43 @@
 class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find params[:id]
+  end
+
+  def new 
+  end
+
+  def edit
+    @id = params[:id]
+  end
+
+  def update
+    photo = Photo.find params[:id]
+    photo.title = params[:photo][:title]
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+    redirect_to photo
+  end
+
+  def create
+    photo = Photo.new
+    photo.title = params[:photo][:title]
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+    
+    redirect_to photo
+  end
+
+  def destroy
+    photo = Photo.find params[:id]
+    photo.destroy
+
+    redirect_to photo
+  end
 end
